0d5f5a6dff1bfb21e7c42e09005cadb263c818b0
molchanova-varya <162100635+molchanova-varya@users.noreply.github.com>
2024-03-25 14:20:16 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,20 +1,17 @@
-// Инициализация карты
-var mymap = L.map('map').setView([51.505, -0.09], 2.5);
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map('map').setView([51.505, -0.09], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(mymap);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-L.marker([55.803688377351406, 37.41016499204887]).addTo(mymap)
-    .bindPopup('Я тута')
-    .openPopup();
+    var marker3 = L.marker([55.80333861300149, 37.409907497317356]).addTo(map);
+    marker3.bindPopup("<b>Молодец!</b><br>Я тута.").openPopup();
 
-L.marker([7.957632284101993, 98.33758940527918]).addTo(mymap)
-    .bindPopup('И даже не здесь!')
-    .openPopup();
+    var marker2 = L.marker([7.93123809460827, 98.32667234059166]).addTo(map);
+    marker2.bindPopup("<b>Ищешь меня?</b><br>И я даже не здесь!").openPopup();
 
-L.marker([51.51, -0.2]).addTo(mymap)
-    .bindPopup('Привет! Я, увы, не здесь.')
-    .openPopup();
+    var marker1 = L.marker([51.5, -0.09]).addTo(map);
+    marker1.bindPopup("<b>Привет! </b><br>Я, увы, не здесь.").openPopup();
 
-    
+});
